ccd17a09d4405362bbd659afaa2a7aadc21fabe1
KingOlli <KingOlli@Olivers-MacBook-Air.local>
2016-05-22 13:22:24 +0200

    Scripts PlayerInputManger/PlayerManager/TriggerControler
    Nachtrag  wegen Fehler  zwischen iMac und MacBook

---
+++ ST/Assets/Studienleistung/Scripts/PlayerInputManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInputManager : MonoBehaviour {
+
+	private Rigidbody m_rigidbody;
+
+	public float speedBoost = 5.0f;
+	public float rotationBoost = 50.0f;
+	private string m_MovementAxis = "Move";
+	private string m_RotationAxis = "Rotate";
+	private float m_MovementInput;
+	private float m_RoationInput;
+
+
+
+	//Tank-Tutorial
+
+	// Use this for initialization
+	void Start () {
+		Init ();
+	}
+
+	private void Move() {
+
+	}
+
+	private void Rotate() {
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+	}
+}
