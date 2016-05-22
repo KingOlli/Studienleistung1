5e4c99fcb1808b3e159db1de4da32e785415d5d9
KingOlli <KingOlli@Olivers-MacBook-Air.local>
2016-05-22 13:04:58 +0200

    DoorScript
    start with the DoorScript

---
+++ ST/Assets/Studienleistung/Scripts/Door.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	public bool isClosed = true;
+	public float unlockDepth = 0.0f;
+	public float closeDepth = 2.0f;
+	public float closeSpeed = 3f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate() {
+		if (isClosed == true) {
+				openDoor ();
+			} else {
+				closeDoor();
+			}
+	}
+
+	void openDoor() {
+
+	}
+
+
+	void closeDoor() {
+
+	}
+	
+	
+}
