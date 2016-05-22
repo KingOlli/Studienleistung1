179f5e74d9f15787ed3e294197b3305d6c9aba68
KingOlli <KingOlli@Olivers-MacBook-Air.local>
2016-05-22 16:01:07 +0200

    PlayerInputManager
    PlayerInputManager implementiert

--- ST/Assets/Studienleistung/Scripts/PlayerInputManager.cs
+++ ST/Assets/Studienleistung/Scripts/PlayerInputManager.cs
@@ -21,16 +21,30 @@ public class PlayerInputManager : MonoBehaviour {
 		Init ();
 	}
 
+	void Init() {
+		m_rigidbody = gameObject.GetComponent<Rigidbody> ();
+	}
+
 	private void Move() {
+		m_MovementInput = Input.GetAxis (m_MovementAxis);
+
+		Vector3 movement = transform.forward * speedBoost * Time.deltaTime;
+		m_rigidbody.MovePosition (m_rigidbody.position + movement);
 
 	}
 
 	private void Rotate() {
+		m_RoationInput = Input.GetAxis (m_RotationAxis);
 
+		float turn = m_RoationInput * rotationBoost * Time.deltaTime;
+		Quaternion rotation = Quaternion.Euler (0f, turn, 0f);
+		m_rigidbody.MoveRotation (m_rigidbody.rotation * rotation);
+	
 	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
-
+		Move ();
+		Rotate ();
 	}
 }
